ff8cab779f92d00369be3d16cf2b10f3ec193207
Reali22 <zazaahmed1516@gmail.com>
2025-12-11 00:18:52 +0300

    Update plugin.py

--- plugin.py
+++ plugin.py
@@ -23,7 +23,7 @@ except ImportError:
 
 # --- CONFIGURATION & CONSTANTS ---
 CONFIG_FILE = "/etc/enigma2/footscores_config.json"
-PLUGIN_VERSION = "1.8" # Updated for Green Goal Banner
+PLUGIN_VERSION = "1.1" # Updated for Green Goal Banner
 
 # DIRECT LINKS TO YOUR REPO
 UPDATE_URL = "https://raw.githubusercontent.com/Ahmed-Mohammed-Abbas/FootScores/main/version.txt"
